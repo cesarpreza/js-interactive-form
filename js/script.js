52266cc4309d552488d5b40be3ad2785e65de0b6
Cesar Preza <cpreza93@yahoo.com>
2023-01-10 20:56:36 -0800

    adds listener for user name

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-document.querySelector('#submit-button').addEventListener('click', e => {
-    e.preventDefault;
+//Autofocus on the name input field on load of the form. 
+userName = document.querySelector('#name');
+userName.focus();
+
+document.querySelector('#other-job-role').type = 'hidden';
+
+userName.addEventListener('input', function(e) {
+    userName = e.target.value;
+    console.log(userName)
 })
+
+
+// function selectJobRole() {
+//     let jobTitle = document.querySelector('#title').value;
+//     let selectOption = document.querySelector('select');
+    
+//     selectOption.addEventlistener('onChange', e => {
+//         console.log(selectOption);
+//     })
+//     console.log(jobTitle, selectOption);
+// }
+
+
+// document.querySelector('#submit-button').addEventListener('click', e => {
+//     e.preventDefault;
+// })
+//selectJobRole();
